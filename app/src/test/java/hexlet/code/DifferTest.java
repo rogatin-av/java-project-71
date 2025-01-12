b5d8f560f94af46d76fd3eadaab42afe932bb10d
Andrey <rogatin-av@ya.ru>
2025-01-12 12:36:40 +0300

    Update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,8 +1,10 @@
 package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,11 +12,11 @@ import java.nio.file.Paths;
 
 public class DifferTest {
 
-    private static final String PATH_TO_JSON1 = "src/test/resources/file1.json";
-    private static final String PATH_TO_JSON2 = "src/test/resources/file2.json";
-    private static final String PATH_TO_YAML1 = "src/test/resources/file1.yaml";
-    private static final String PATH_TO_YAML2 = "src/test/resources/file2.yaml";
-    private static final String STYILISH_FORMAT = "stylish";
+    private static final String FILE1 = "file1";
+    private static final String FILE2 = "file2";
+    private static final String JSON_EXC = ".json";
+    private static final String YAML_EXC = ".yaml";
+    private static final String STYLISH_FORMAT = "stylish";
     private static final String PLAIN_FORMAT = "plain";
     private static final String JSON_FORMAT = "json";
     private static String stylish;
@@ -33,71 +35,55 @@ public class DifferTest {
 
     @BeforeAll
     public static void beforeAll() throws Exception {
-        stylish = getFixtureContent(STYILISH_FORMAT);
+        stylish = getFixtureContent(STYLISH_FORMAT);
         plain = getFixtureContent(PLAIN_FORMAT);
         json = getFixtureContent(JSON_FORMAT);
     }
 
-    @Test
-    void testJsonToStylish() throws Exception {
-        String actual = Differ.generate(PATH_TO_JSON1, PATH_TO_JSON2, STYILISH_FORMAT);
+    @ParameterizedTest
+    @ValueSource(strings = { JSON_EXC, YAML_EXC })
+    void testDefault(String fileException) throws Exception {
+        String filepath1 = getFixturePath(FILE1 + fileException).toString();
+        String filepath2 = getFixturePath(FILE2 + fileException).toString();
+
+        String actual = Differ.generate(filepath1, filepath2);
         String expected = stylish;
 
         assertEquals(expected, actual);
     }
 
-    @Test
-    void testJsonToPlain() throws Exception {
-        String actual = Differ.generate(PATH_TO_JSON1, PATH_TO_JSON2, PLAIN_FORMAT);
+    @ParameterizedTest
+    @ValueSource(strings = { JSON_EXC, YAML_EXC })
+    void testStylish(String fileException) throws Exception {
+        String filepath1 = getFixturePath(FILE1 + fileException).toString();
+        String filepath2 = getFixturePath(FILE2 + fileException).toString();
+
+        String actual = Differ.generate(filepath1, filepath2, STYLISH_FORMAT);
+        String expected = stylish;
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { JSON_EXC, YAML_EXC })
+    void testPlain(String fileException) throws Exception {
+        String filepath1 = getFixturePath(FILE1 + fileException).toString();
+        String filepath2 = getFixturePath(FILE2 + fileException).toString();
+
+        String actual = Differ.generate(filepath1, filepath2, PLAIN_FORMAT);
         String expected = plain;
 
         assertEquals(expected, actual);
     }
 
-    @Test
-    void testJsonToJson() throws Exception {
-        String actual = Differ.generate(PATH_TO_JSON1, PATH_TO_JSON2, JSON_FORMAT);
+    @ParameterizedTest
+    @ValueSource(strings = { JSON_EXC, YAML_EXC })
+    void testJson(String fileException) throws Exception {
+        String filepath1 = getFixturePath(FILE1 + fileException).toString();
+        String filepath2 = getFixturePath(FILE2 + fileException).toString();
+
+        String actual = Differ.generate(filepath1, filepath2, JSON_FORMAT);
         String expected = json;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testJsonToDefault() throws Exception {
-        String actual = Differ.generate(PATH_TO_JSON1, PATH_TO_JSON2);
-        String expected = stylish;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testYamlToStylish() throws Exception {
-        String actual = Differ.generate(PATH_TO_YAML1, PATH_TO_YAML2, STYILISH_FORMAT);
-        String expected = stylish;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testYamlToPlain() throws Exception {
-        String actual = Differ.generate(PATH_TO_YAML1, PATH_TO_YAML2, PLAIN_FORMAT);
-        String expected = plain;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testYamlToJson() throws Exception {
-        String actual = Differ.generate(PATH_TO_YAML1, PATH_TO_YAML2, JSON_FORMAT);
-        String expected = json;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void testYamlToDefault() throws Exception {
-        String actual = Differ.generate(PATH_TO_YAML1, PATH_TO_YAML2);
-        String expected = stylish;
 
         assertEquals(expected, actual);
     }
